f399f032826e421bf47c9e0ecbaede9975c42ec0
Unknown <Jordan.Selinger@gmail.com>
2018-05-19 00:48:24 -0600

    Imported libraries and quickstart

--- GoogleSheetsApiDriver/app/src/main/java/com/example/jordan/googlesheetsapidriver/Program.java
+++ GoogleSheetsApiDriver/app/src/main/java/com/example/jordan/googlesheetsapidriver/Program.java
@@ -1,8 +1,24 @@
 package com.example.jordan.googlesheetsapidriver;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
 public class Program {
     public static void main(String[] args)
     {
+        SheetsQuickstart quickstart = new SheetsQuickstart();
 
+        try
+        {
+            quickstart.DoThings();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        catch (GeneralSecurityException e)
+        {
+            e.printStackTrace();
+        }
     }
 }
